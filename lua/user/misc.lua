40b422367c3f1ba5458a3580316b8ab42a0f524b
rjshrjndrn <rjshrjndrn@gmail.com>
2022-07-12 23:33:49 +0200

    chore(lua): revert clipboard from system after 1s
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/user/misc.lua
+++ lua/user/misc.lua
@@ -5,14 +5,21 @@ local opts = { silent = false }
 
 -- Ref: https://github.com/nanotee/nvim-lua-guide#vlua
 local function clipboardToggle()
-  local clipboardValue = vim.api.nvim_get_option('clipboard')
-  if (clipboardValue == 'unnamedplus') then
-    vim.api.nvim_set_option_value('clipboard', '' ,{scope = 'global'})
-    print('clipboard: ')
-  else
-    vim.api.nvim_set_option_value('clipboard', 'unnamedplus', {scope = 'global'})
+    vim.api.nvim_set_option_value('clipboard', 'unnamedplus' ,{scope = 'global'})
+    local timer = vim.loop.new_timer()
     print('clipboard: unnamedplus')
-  end
+    -- Wait for 1s and revert to the old clipboard
+    -- :help timer_start
+    timer:start(1000,0, vim.schedule_wrap(function ()
+      -- TODO: Have to check how to automatically, revert if a key is pressed
+      vim.api.nvim_set_option_value('clipboard', '' ,{scope = 'global'})
+      print('clipboard: ')
+    end
+    ))
+    -- -- print('clipboard: ')
 end
 
 keymap("n", "<leader>c",clipboardToggle, opts)
+keymap("v", "<leader>c",clipboardToggle, opts)
+
+
