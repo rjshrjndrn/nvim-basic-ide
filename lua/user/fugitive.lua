72c33af9471c6735aff38aabaf030ac4a999e2d2
rjshrjndrn <rjshrjndrn@gmail.com>
2022-10-11 10:46:08 +0200

    chore(config): Adding plugins and config

--- lua/user/fugitive.lua
+++ lua/user/fugitive.lua
@@ -5,4 +5,6 @@ local opts = { silent = true }
 
 keymap("n", "gw", "<cmd>Gwrite<CR>", opts)
 keymap("n", "gc", "<cmd>Git commit<CR>", opts)
+keymap("n", "gca", "<cmd>Git commit --amend<CR>", opts)
 keymap("n", "gs", "<cmd>Git<CR>", opts)
+keymap("n", "gp", "<cmd>Git push<CR>", opts)
