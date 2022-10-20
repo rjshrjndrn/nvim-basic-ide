4df0fe5fefd1fc82ab5d3c98f9c59bb198a9a058
rjshrjndrn <rjshrjndrn@gmail.com>
2022-10-20 05:47:22 +0200

    chore(toggleterm): change toggle to alt-.
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -3,22 +3,22 @@ if not status_ok then
   return
 end
 
-toggleterm.setup {
-  size = 20,
-  open_mapping = [[<c-\>]],
-  hide_numbers = true,
-  shade_terminals = true,
-  shading_factor = 2,
-  start_in_insert = true,
-  insert_mappings = true,
-  persist_size = true,
-  direction = "float",
-  close_on_exit = true,
-  shell = vim.o.shell,
-  float_opts = {
-    border = "curved",
-  },
-}
+toggleterm.setup({
+	size = 20,
+	open_mapping = [[<a-.>]],
+	hide_numbers = true,
+	shade_terminals = true,
+	shading_factor = 2,
+	start_in_insert = true,
+	insert_mappings = true,
+	persist_size = true,
+	direction = "float",
+	close_on_exit = true,
+	shell = vim.o.shell,
+	float_opts = {
+		border = "curved",
+	},
+})
 
 function _G.set_terminal_keymaps()
   local opts = { noremap = true }
