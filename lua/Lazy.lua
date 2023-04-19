b18db9f5b520bff021d04ac0008c9440db425687
rjshrjndrn <rjshrjndrn@gmail.com>
2023-04-19 16:51:18 +0200

    chore(lazy): Disable notification for update
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/Lazy.lua
+++ lua/Lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup("user", {
   install = { colorscheme = { require("user.colorscheme").name } },
   defaults = { lazy = true, version = "57cce98dfdb2f2dd05a0567d89811e6d0505e13b" },
   ui = { wrap = "true" },
+  checker = { enabled = true, notify = false },
   change_detection = { enabled = true, notify = false },
   checker = { enabled = true },
   debug = false,
