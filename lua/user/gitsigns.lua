f543a01ace6f49b71669690daa1850ae831dabf1
rjshrjndrn <rjshrjndrn@gmail.com>
2023-05-19 15:17:21 +0200

    chore(nvim): Adding gitblame
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -18,6 +18,7 @@ M.opts = {
     follow_files = true,
   },
   attach_to_untracked = true,
+  current_line_blame = true,
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
