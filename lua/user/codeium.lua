c67d9c9855d42ed3fa2465915acb1d55c0579851
rjshrjndrn <rjshrjndrn@gmail.com>
2023-11-08 16:49:07 +0100

    plugins: Adding official codeium plugin
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/user/codeium.lua
+++ lua/user/codeium.lua
@@ -1,6 +1,5 @@
 local M = {
-    "rjshrjndrn/codeium.nvim",
-    -- commit = "2486e4945f2210546c6d51d0bf60a0350bacd406",
+    "Exafunction/codeium.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "hrsh7th/nvim-cmp",
