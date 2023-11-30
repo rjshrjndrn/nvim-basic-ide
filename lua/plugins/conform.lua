c2bb3c907e95cb9c59df143a2ac6d21be71c1d6b
rjshrjndrn <rjshrjndrn@gmail.com>
2023-11-30 15:44:35 +0100

    feat(format): format bash with indent
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,13 @@
+return {
+  "stevearc/conform.nvim",
+  -- Ref: https://github.com/stevearc/conform.nvim/issues/201#issuecomment-1813007081
+  opts = {
+    -- Format bash using space rather than tab
+    formatters = {
+      shfmt_i = { command = "shfmt", args = { "-i", "2" } },
+    },
+    formatters_by_ft = {
+      sh = { "shfmt" },
+    },
+  },
+}
