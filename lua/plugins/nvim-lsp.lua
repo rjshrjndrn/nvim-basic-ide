9ea86a43dbed4ee93649cf7d64637e8cd73d0921
rjshrjndrn <rjshrjndrn@gmail.com>
2023-12-05 06:56:29 +0100

    disable yamlls diagnostics message
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -25,10 +25,25 @@ return {
     setup = {
       yamlls = function(server, opts)
         opts.on_attach = function(client, bufnr)
+          -- Debug: Open /tmp/file in append mode
+          -- -- Get the filetype of the current buffer
+          -- local filetype = vim.bo[bufnr].filetype
+          -- local filename = vim.api.nvim_buf_get_name(bufnr)
+          -- local file = io.open("/tmp/file", "a")
+          -- if file then
+          --   -- Write buffer number and filetype to the file
+          --   file:write(
+          --     "Buffer Number: " .. bufnr .. ", Filetype: " .. filetype .. "\n",
+          --     "FileName: " .. filename .. "\n"
+          --   )
+          --   -- Close the file
+          --   file:close()
+          -- end
+
           -- Disable diagnostics for Helm file types
-          if vim.bo[bufnr].filetype == "helm" then
-            vim.diagnostic.disable(bufnr)
-          end
+          -- if vim.bo[bufnr].filetype == "helm" then
+          vim.diagnostic.disable(bufnr)
+          -- end
         end
         require("lspconfig")[server].setup(opts)
       end,
