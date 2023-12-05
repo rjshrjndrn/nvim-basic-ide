1ee4b757bb1e276eea1acd571d0d2ae805ee8c1b
rjshrjndrn <rjshrjndrn@gmail.com>
2023-12-05 05:46:26 +0100

    feat(lsp): disable yaml formatting for helm file type
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -8,15 +8,30 @@ return {
       terraformls = {
         filetypes = { "tf", "terraform", "hcl", "terraform-vars" },
       },
+      helm_ls = {
+        filetypes = { "helm" },
+      },
       yamlls = {
         filetypes = { "yml", "yaml", "helm" },
       },
-      bashls = {},
+      bashls = {
+        filetypes = { "sh", "bash" },
+      },
       tflint = {},
       gopls = {},
       pyright = {},
       jsonls = {},
-      helm_ls = {},
+    },
+    setup = {
+      yamlls = function(server, opts)
+        opts.on_attach = function(client, bufnr)
+          -- Disable diagnostics for Helm file types
+          if vim.bo[bufnr].filetype == "helm" then
+            vim.diagnostic.disable(bufnr)
+          end
+        end
+        require("lspconfig")[server].setup(opts)
+      end,
     },
   },
 }
