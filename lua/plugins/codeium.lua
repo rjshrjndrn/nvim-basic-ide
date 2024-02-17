f64997cccf33c7e0630aa487ab0f2bac99518e84
rjshrjndrn <rjshrjndrn@gmail.com>
2024-02-17 08:06:13 +0100

    nvim: update codeium priority
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,13 +1,20 @@
 return {
-  -- codeium cmp source
-  {
-    "nvim-cmp",
-    dependencies = {
-      -- codeium
-      {
-        "Exafunction/codeium.nvim",
-        lazy = false,
-      },
+  "nvim-cmp",
+  dependencies = {
+    -- codeium
+    {
+      "Exafunction/codeium.nvim",
+      cmd = "Codeium",
+      -- build = ":Codeium Auth",
+      opts = {},
     },
   },
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    table.insert(opts.sources, 1, {
+      name = "codeium",
+      group_index = 1,
+      priority = 10,
+    })
+  end,
 }
