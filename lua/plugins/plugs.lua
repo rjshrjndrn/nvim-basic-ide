8cb965907b06bd877f55f31413622fa6be017b3a
rjshrjndrn <rjshrjndrn@gmail.com>
2024-06-04 13:30:19 +0200

    chore(plugin): lua unimpared
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/plugs.lua
+++ lua/plugins/plugs.lua
@@ -1,3 +1,10 @@
 return {
   { "towolf/vim-helm", lazy = true, ft = "yaml" },
+  {
+    "tummetott/unimpaired.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add options here if you wish to override the default settings
+    },
+  },
 }
