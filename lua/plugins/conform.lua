058cd8e176fea5d59daf1a7d8877d72b0b8f5ce8
rjshrjndrn <rjshrjndrn@gmail.com>
2024-06-08 16:29:26 +0200

    adding plugin

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+  "stevearc/conform.nvim",
+  -- Ref: https://github.com/stevearc/conform.nvim/issues/201#issuecomment-1813007081
+  opts = {
+    -- Format bash using space rather than tab
+    formatters = {
+      shfmt_i = { command = "shfmt", args = { "-i", "4" } },
+    },
+    formatters_by_ft = {
+      sh = { "shfmt_i" },
+      -- Conform will run multiple formatters sequentially
+      python = { "isort", "black" },
+    },
+  },
+}
