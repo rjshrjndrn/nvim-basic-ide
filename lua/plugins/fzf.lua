940bebda7828ec37b53e61b96f9ddd806d5e4652
rjshrjndrn <rjshrjndrn@gmail.com>
2024-06-26 23:07:35 +0200

    fzf: per picker config
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -8,15 +8,30 @@ return {
   },
   opts = function(_, opts)
     local config = require("fzf-lua.config")
-    config.defaults.actions.files["ctrl-t"] = require("fzf-lua.actions").file_tabedit
-    config.defaults.keymap.builtin["<a-f>"] = "toggle-fullscreen"
-    config.defaults.keymap.builtin["<c-w>"] = "toggle-preview-wrap"
-    config.defaults.keymap.builtin["<c-p>"] = "toggle-preview"
-    opts.winopts = {
+    local defaults = require("fzf-lua.profiles.default-title")
+    local file_win_opts = {
       preview = {
         layout = "vertical",
         vertical = "up:70%",
       },
     }
+    config.defaults.actions.files["ctrl-t"] = require("fzf-lua.actions").file_tabedit
+    config.defaults.keymap.builtin["<a-f>"] = "toggle-fullscreen"
+    config.defaults.keymap.builtin["<c-w>"] = "toggle-preview-wrap"
+    config.defaults.keymap.builtin["<c-p>"] = "toggle-preview"
+    return vim.tbl_deep_extend("force", defaults, {
+      defaults = {
+        -- formatter = "path.filename_first",
+        formatter = "path.filename_first",
+      },
+      files = {
+        winopts = file_win_opts,
+      },
+      git = {
+        files = {
+          winopts = file_win_opts,
+        },
+      },
+    })
   end,
 }
