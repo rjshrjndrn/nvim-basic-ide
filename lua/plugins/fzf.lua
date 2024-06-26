f5767f6d5b4af08c7127584f28b06547ebb41ad3
rjshrjndrn <rjshrjndrn@gmail.com>
2024-06-26 15:28:19 +0200

    fzf keymaps
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,9 +3,14 @@ return {
   keys = {
     { "//", LazyVim.pick("grep_cword"), desc = "Word (Root Dir)" },
     { "//", LazyVim.pick("grep_visual"), mode = "v", desc = "Selection (Root Dir)" },
+    { "??", LazyVim.pick("grep_cword", { root = false }), desc = "Word (Root Dir)" },
+    { "??", LazyVim.pick("grep_visual", { root = false }), mode = "v", desc = "Selection (Root Dir)" },
   },
   opts = function(_, opts)
     local config = require("fzf-lua.config")
     config.defaults.actions.files["ctrl-t"] = require("fzf-lua.actions").file_tabedit
+    config.defaults.keymap.builtin["<a-f>"] = "toggle-fullscreen"
+    config.defaults.keymap.builtin["<c-w>"] = "toggle-preview-wrap"
+    config.defaults.keymap.builtin["<c-p>"] = "toggle-preview"
   end,
 }
