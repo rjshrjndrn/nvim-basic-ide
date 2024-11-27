382ab92965160277e6cbfb5f8e73de17db071bed
rjshrjndrn <rjshrjndrn@gmail.com>
2024-11-27 15:15:32 +0100

    chore(plugin): open terminal
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -12,7 +12,7 @@ function M.config()
 
   toggleterm.setup({
     size = 20,
-    open_mapping = [[<a-.>]],
+    open_mapping = [[<leader>.]],
     hide_numbers = true,
     shade_terminals = true,
     shading_factor = 2,
