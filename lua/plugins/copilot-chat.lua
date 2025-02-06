1629c0068360e501f033300a2070c662a409f630
rjshrjndrn <rjshrjndrn@gmail.com>
2025-02-06 10:59:33 +0100

    adding copilot with sonnet

---
+++ lua/plugins/copilot-chat.lua
@@ -0,0 +1,9 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  opts = {
+    model = "claude-3.5-sonnet",
+    prompts = {
+      Explain = "Write an comprehensive explanation for the selected code.",
+    },
+  },
+}
