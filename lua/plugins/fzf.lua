e3288019f617b75e0e1ea033a955294058617e6a
rjshrjndrn <rjshrjndrn@gmail.com>
2025-02-12 11:33:35 +0100

    chore(fzf): override config
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -3,33 +3,42 @@ return {
   keys = {
     { "<leader>gs", false },
     { "<leader>gc", false },
-    --   { "//", LazyVim.pick("grep_cword"), desc = "Word (Root Dir)" },
-    --   { "//", LazyVim.pick("grep_visual"), mode = "v", desc = "Selection (Root Dir)" },
-    --   { "??", LazyVim.pick("grep_cword", { root = false }), desc = "Word (CW Dir)" },
-    --   { "??", LazyVim.pick("grep_visual", { root = false }), mode = "v", desc = "Selection (CW Dir)" },
   },
-  opts = function()
+  --   Example of how it works:
+  -- ```lua
+  -- opts = function(_, opts)
+  --   -- opts contains any default/existing options
+  --   -- modify the opts
+  --   opts.new_setting = "value"
+  --   -- the modified opts will be used to configure the plugin
+  --   return opts
+  -- end
+  -- ```
+  opts = function(_, opts)
     local config = require("fzf-lua.config")
     local actions = require("fzf-lua.actions")
-    --   -- Refer: https://github.com/ibhagwan/fzf-lua/blob/main/lua/fzf-lua/defaults.lua
-    --   local file_win_opts = {
-    --     fullscreen = false,
-    --     preview = {
-    --       layout = "vertical",
-    --       vertical = "up:70%",
-    --     },
-    --   }
+
+    -- Refer: https://github.com/ibhagwan/fzf-lua/blob/main/lua/fzf-lua/defaults.lua
+    local file_win_opts = {
+      fullscreen = false,
+      preview = {
+        hidden = "hidden",
+      },
+    }
+
     config.defaults.actions.files["ctrl-t"] = actions.file_tabedit
-    --   config.defaults.keymap.builtin["<a-f>"] = "toggle-fullscreen"
-    --   config.defaults.keymap.builtin["<c-w>"] = "toggle-preview-wrap"
-    --   config.defaults.keymap.builtin["<c-p>"] = "toggle-preview"
-    --   opts.files = vim.tbl_deep_extend("force", opts.files, { winopts = file_win_opts })
-    --   opts.grep.actions = {
-    --     ["alt-g"] = { actions.toggle_ignore },
-    --     ["alt-i"] = { actions.toggle_hidden },
-    --   }
-    --   opts.oldfiles = vim.tbl_deep_extend("force", opts.oldfiles, { winopts = file_win_opts })
-    --   opts.buffers = vim.tbl_deep_extend("force", opts.oldfiles, { winopts = file_win_opts })
-    --   opts.git.files = vim.tbl_deep_extend("force", opts.git.files, { winopts = file_win_opts })
+    config.defaults.keymap.builtin["<c-w>"] = "toggle-preview-wrap"
+    config.defaults.keymap.builtin["<c-p>"] = "toggle-preview"
+    config.defaults.keymap.builtin["<c-o>"] = "toggle-help"
+
+    -- Set global default preview to hidden
+    config.defaults.winopts = vim.tbl_deep_extend("force", config.defaults.winopts, file_win_opts)
+
+    -- Apply hidden preview to specific commands
+    opts.oldfiles = vim.tbl_deep_extend("force", opts.oldfiles or {}, { winopts = file_win_opts })
+    opts.buffers = vim.tbl_deep_extend("force", opts.buffers or {}, { winopts = file_win_opts })
+    opts.git = vim.tbl_deep_extend("force", opts.git or {}, {
+      files = vim.tbl_deep_extend("force", opts.git and opts.git.files or {}, { winopts = file_win_opts }),
+    })
   end,
 }
