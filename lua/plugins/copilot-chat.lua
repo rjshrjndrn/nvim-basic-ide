254fd095510809a27ac5ceff35015c35024cf579
rjshrjndrn <rjshrjndrn@gmail.com>
2025-02-26 02:47:53 +0100

    chore(plugin): Adding 3.7-sonnet for copilot-chat
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -1,7 +1,7 @@
 return {
   "CopilotC-Nvim/CopilotChat.nvim",
   opts = {
-    model = "claude-3.5-sonnet",
+    model = "claude-3.7-sonnet",
     prompts = {
       Explain = "Write an comprehensive explanation for the selected code.",
     },
