c2fbef17d8a88460cf91b24144472dd881e8695a
rjshrjndrn <rjshrjndrn@gmail.com>
2025-03-05 05:35:00 +0100

    feat(toggleterm): enable start_in_insert mode by default
    When opening terminal with toggleterm, automatically start in insert mode
    to improve workflow efficiency and allow immediate typing without
    requiring additional keystrokes to enter insert mode.
    
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -16,7 +16,7 @@ function M.config()
     hide_numbers = true,
     shade_terminals = true,
     shading_factor = 2,
-    start_in_insert = false,
+    start_in_insert = true,
     insert_mappings = true,
     persist_size = true,
     direction = "float",
