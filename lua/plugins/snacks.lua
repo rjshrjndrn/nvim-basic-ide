ddbc835e8c14c2c439f61c1fc0ff7943ce45e00c
rjshrjndrn <rjshrjndrn@gmail.com>
2025-03-06 11:34:25 +0100

    feat(snacks): add commit hash yanking and file visibility
    - Add key binding "yc" to yank commit hash to clipboard
    - Configure picker to show hidden and ignored files by default
    - Fix unused variable diagnostic in git_browse function
    
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -15,6 +15,10 @@ return {
             "git_browse",
             mode = { "n" },
           },
+          ["yc"] = {
+            "yank_commit",
+            mode = { "n" },
+          },
         },
       },
     }
@@ -36,7 +40,16 @@ return {
     local gitbrowse = require("snacks.gitbrowse")
     return vim.tbl_deep_extend("force", opts or {}, t_picker, {
       picker = {
+        -- Ref: https://github.com/LazyVim/LazyVim/discussions/5723
+        -- show hidden files like .env
+        hidden = true,
+        -- show files ignored by git like node_modules
+        ignored = true,
         sources = {
+          files = {
+            ignored = true,
+            hidden = true,
+          },
           git_log = {
             win = preview_window_opts,
           },
@@ -52,6 +65,12 @@ return {
             -- picker:close()
             -- vim.notify(vim.inspect(item), vim.log.levels.INFO)
             return gitbrowse.open({ what = "commit", commit = item.commit })
+          end,
+          yank_commit = function(_, item)
+            if item and item.commit then
+              vim.fn.setreg("+", item.commit)
+              vim.notify("Commit hash yanked: " .. item.commit, vim.log.levels.INFO)
+            end
           end,
         },
         win = {
