e8dec2cdabadd45fbb23cb62f7da52f7f5784ca8
rjshrjndrn <rjshrjndrn@gmail.com>
2025-03-29 04:42:21 +0100

    feat(security): disable undo and backups for encrypted files
    Add autocommand to protect sensitive data in encrypted files by
    disabling persistent undo, swap files, and backup files when editing
    *.age, *.gpg, *.enc, and *.encrypted files.
    
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Disable undo for specific file types (like encrypted files)
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.age", "*.gpg", "*.enc", "*.encrypted" },
+  callback = function()
+    vim.opt_local.undofile = false -- Disable persistent undo
+    -- vim.opt_local.undolevels = -1 -- Disable undo completely for the buffer
+    vim.opt_local.swapfile = false -- Also good to disable swap for encrypted files
+    vim.opt_local.backup = false -- No backup files
+    vim.opt_local.writebackup = false -- No backup while writing
+  end,
+})
