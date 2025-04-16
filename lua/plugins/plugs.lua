4f3b42239660ded168cb69eb4a179a536f9e0355
rjshrjndrn <rjshrjndrn@gmail.com>
2025-04-16 09:17:35 +0200

    TODO: remove custom upstream once the PR merged
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/plugs.lua
+++ lua/plugins/plugs.lua
@@ -7,4 +7,11 @@ return {
       -- add options here if you wish to override the default settings
     },
   },
+  {
+    "junegunn/vim-easy-align",
+  },
+  {
+    "rjshrjndrn/friendly-snippets",
+    branch = "patch-1",
+  },
 }
