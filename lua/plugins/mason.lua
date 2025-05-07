4fc1c5fabc7db40d640c729c71c6a50e47684028
rjshrjndrn <rjshrjndrn@gmail.com>
2025-05-07 08:37:32 +0200

    tmp: workaround for upstream breaking change
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,10 +1,14 @@
 return {
-  "williamboman/mason.nvim",
-  ensure_installed = {
-    "stylua",
-    "shfmt",
-    "helm_ls",
-    "hcl",
-    "gopls",
+  { "mason-org/mason.nvim", version = "1.11.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
+  {
+    "williamboman/mason.nvim",
+    ensure_installed = {
+      "stylua",
+      "shfmt",
+      "helm_ls",
+      "hcl",
+      "gopls",
+    },
   },
 }
